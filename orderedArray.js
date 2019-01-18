a8512331ddeb307a8f52200309be33b04f9e5a31
Srecko Jarcevic <sreckojarcevic@gmail.com>
2019-01-18 11:18:42 +0100

    Update orderedArray.js

--- orderedArray.js
+++ orderedArray.js
@@ -1,1 +1,23 @@
+// data structure ordered array
+// adding item result looping through arr until finding index of item with higher value
+// always removing last value (max value)
+// algrorithm complexity - adding O(n), removing O(1)
 
+const root = document.getElementById("app");
+
+class orderedArrayAl {
+  constructor(data) {
+    this.data = data.sort((a, b) => a - b);
+  }
+  insert(item) {
+    const pushIndex = this.data.findIndex(member => member > item);
+    if (pushIndex === -1) {
+      this.data.push(item);
+    } else {
+      this.data.splice(pushIndex, 0, item);
+    }
+  }
+  removeMax() {
+    this.data.pop();
+  }
+}
